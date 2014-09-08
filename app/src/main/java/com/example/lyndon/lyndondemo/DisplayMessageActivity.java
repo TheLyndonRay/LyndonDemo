46de025fc9fb09d398ae4ec89ac1adbd80956137
Lyndon R. Labayan <lyndon.labayan@gmail.com>
2014-09-08 09:47:58 -0500

    4th commit - Completed Building First App section

--- app/src/main/java/com/example/lyndon/lyndondemo/DisplayMessageActivity.java
+++ app/src/main/java/com/example/lyndon/lyndondemo/DisplayMessageActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.Intent;
+import android.widget.TextView;
 
 
 public class DisplayMessageActivity extends Activity {
@@ -12,6 +14,18 @@ public class DisplayMessageActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
+
+        //get the intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Create the text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        // Set the text view as the activity layout
+        setContentView(textView);
     }
 
 
@@ -33,4 +47,6 @@ public class DisplayMessageActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
 }
