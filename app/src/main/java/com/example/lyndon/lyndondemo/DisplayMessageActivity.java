17d21c3b96285353998d924798c483adf4da189b
Lyndon R. Labayan <lyndon.labayan@gmail.com>
2014-09-09 21:34:42 -0500

    7th commit - Created the first elements of an action bar

--- app/src/main/java/com/example/lyndon/lyndondemo/DisplayMessageActivity.java
+++ app/src/main/java/com/example/lyndon/lyndondemo/DisplayMessageActivity.java
@@ -6,9 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.content.Intent;
 import android.widget.TextView;
-//
-//
-//
+
 
 public class DisplayMessageActivity extends Activity {
 
